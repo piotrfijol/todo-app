92c24dd0d4f5a38fbe116e080c99a90a85bc67ea
Piotr Fijol <piotr.fijol1@gmail.com>
2023-03-01 23:42:24 +0100

    Create a darkmode toggle

---
+++ src/components/DarkmodeToggle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="darktoggle">
+        <label @click="handleToggle" for="darkmode-toggle"></label>
+        <input type="checkbox" id="darkmode-toggle">
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            handleToggle() {
+                document.body.classList.toggle('darkmode');
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .darktoggle {
+        
+        input#darkmode-toggle {
+            display: none;
+        }
+
+    }
+</style>
