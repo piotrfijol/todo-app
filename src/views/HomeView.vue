e21bea5132906a9f2608f6dbe0f94538eb85184e
Piotr Fijol <piotr.fijol1@gmail.com>
2023-03-01 21:41:14 +0100

    Create basic HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,37 @@
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home d-flex justify-center align-center flex-column">
+    <h1 class="home__logo">ToDo</h1>
+    <p>Keep your tasks in one place</p>
+    <v-btn
+      color="blue-grey"
+    >
+      Start
+    </v-btn>
+  </div>
 </template>
+
+
+<style scoped lang="scss">
+  .home {
+    transform: translateY(-5rem);
+
+    &__logo, p {
+      font-family: 'Patrick Hand';
+    }
+
+    &__logo {
+      font-size: 128px;
+      font-weight: 800;
+      letter-spacing: .05em;
+    }
+
+    p {
+      margin: .8rem 0;
+      font-size: 24px;
+      letter-spacing: .05em;
+    }
+
+  }
+</style>
