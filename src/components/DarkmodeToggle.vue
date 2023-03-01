27a7d9d98914b9b2f4b19cefe57b445c1ad1dcbd
Piotr Fijol <piotr.fijol1@gmail.com>
2023-03-02 00:09:34 +0100

    Style the darkmode toggle

--- src/components/DarkmodeToggle.vue
+++ src/components/DarkmodeToggle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="darktoggle">
+    <div class="darktoggle" ref="toggle">
         <label @click="handleToggle" for="darkmode-toggle"></label>
         <input type="checkbox" id="darkmode-toggle">
     </div>
@@ -10,6 +10,7 @@
         methods: {
             handleToggle() {
                 document.body.classList.toggle('darkmode');
+                this.$refs.toggle.classList.toggle('toggled');
             }
         },
     }
@@ -18,8 +19,64 @@
 <style lang="scss" scoped>
     .darktoggle {
         
+        & {
+            --toggle-shadow-color: #AAA;
+            --toggle-background: #FFF;
+            
+            width: 40px;
+            height: 17px;
+        }
+
+        &.toggled {
+            --toggle-shadow-color: #231f57;
+            --toggle-background: #443f6e;
+        }
+
         input#darkmode-toggle {
             display: none;
+        }
+
+        
+        label {
+            display: block;
+            position: relative;
+            height: 100%;
+            width: 100%;
+            border-radius: 7px;
+            background-color: var(--toggle-background);
+            box-shadow: inset 0 0 5px 0 var(--toggle-shadow-color);
+        }
+
+        label:hover {
+            cursor: pointer;
+        }
+        
+        /* Toggle circle */
+        label::before, label::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 0;
+            height: 100%;
+            width: 40%;
+            background-color: #FFF;
+            border-radius: 50%;
+            box-shadow: 0 0 2px 0 #DDD;
+            transition: left .15s ease-out;
+        }
+
+        &.toggled label::before {
+            left: 75%;
+        }
+
+        /* Crescent effect */
+        &.toggled label::after {
+            content: '';
+            transform: rotate(45deg) scale3d(0.8, 0.9, 1);
+            left:68%;
+            top: -10%;
+            background-color: var(--toggle-shadow-color);
+            opacity: .9;
         }
 
     }
