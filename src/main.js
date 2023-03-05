21b2e368300484968c1715c5378a6da9888cfb6a
Piotr Fijol <piotr.fijol1@gmail.com>
2023-03-05 10:45:47 +0100

    Add material design icons

--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify/lib/framework.mjs';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 
 const vuetify = createVuetify({
     components,
-    directives
+    directives,
+    aliases,
+    sets: {
+        mdi
+    }
 });
 
 const app = createApp(App)
