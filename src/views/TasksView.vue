73ba6fe22d3306de42d8f74496fd4f182f363a60
Piotr Fijol <piotr.fijol1@gmail.com>
2023-03-05 11:16:21 +0100

    Initialize Tasks view

---
+++ src/views/TasksView.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <header class="header">
+            <h1>Tasks</h1>
+        </header>
+        <div class="tasks">
+            <template v-if="tasks.length">
+                <div 
+                    v-for="task in tasks" 
+                    class="task" 
+                    :key="task.id"
+                    :task="task"
+                    @delete="deleteTask"
+                    @toggle="toggleTaskStatus"
+                ></div>
+            </template>
+            <template v-else>
+                <p>Could not find any tasks.</p>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tasks: []
+            }
+        },
+        mounted () {
+            this.tasks = this.getTasks();
+        },
+        methods: {
+            getTasks() {
+                return [];
+            },
+            saveTasks() {
+            },
+            deleteTask(e, taskId) {
+            },
+            toggleTaskStatus(e, taskId) {
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        margin-top: 6rem;
+        font-size: 1.5rem;
+        text-align: center;
+    }
+
+    p {
+        text-align: center;
+        margin-top: 2rem;
+        opacity: .8;
+    }
+</style>
