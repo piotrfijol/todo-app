1d1e86e06fc8ca872d39408911849262e59942f3
Piotr Fijol <piotr.fijol1@gmail.com>
2023-03-06 13:04:35 +0100

    Create TaskDialog base component

---
+++ src/components/TaskDialog.vue
@@ -0,0 +1,112 @@
+<template>
+    <v-dialog
+        width="100vw"
+        maxWidth="1024px"
+        transition="dialog-top-transition"
+    >
+    
+    <template v-slot:activator="{ props }">
+        <slot name="activator" v-bind="{props}"></slot>
+    </template>
+
+        <v-card>
+            <v-card-title>
+                <span class="text-h5">{{ title }}</span>
+            </v-card-title>
+
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col
+                            cols="12"
+                            xs="12"
+                        >
+                            <v-text-field
+                                label="Title of the task"
+                                v-model="taskTitle"
+                            ></v-text-field>                            
+                        </v-col>
+                        <v-col
+                            cols="12"
+                            xs="12"
+                        >
+                            <v-textarea
+                                label="Description of the task"
+                                v-model="taskDescription"
+                            ></v-textarea>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+                <div class="d-flex">
+                    <v-btn 
+                        @click="handleDialogCancel"
+                        color="dark" 
+                        id="dialog-cancel"
+                        block
+                    >Close</v-btn>
+
+                    <v-btn 
+                        @click="handleDialogSave"
+                        id="dialog-save"
+                        variant="tonal" 
+                        color="info" 
+                        block 
+                        prepend-icon="mdi-plus"    
+                    >{{ saveBtnLabel }}</v-btn>
+                </div>
+            </v-card-actions>
+        </v-card>
+</v-dialog>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: {
+            type: String,
+            default: "Task"
+        },
+        saveBtnLabel: {
+            type: String,
+            default: "Save"
+        },
+        task: {
+            type: Object,
+            default: {
+                id: 0,
+                title: '',
+                description: '',
+                isDone: false
+            }    
+        }
+    },
+    data() {
+        return {
+            taskTitle: this.task.title,
+            taskDescription: this.task.description,
+        }
+    },
+    methods: {
+        handleDialogCancel() {
+            this.$emit('cancel');
+            this.clearForm();
+        },  
+        handleDialogSave() {
+            this.$emit('save', this.task.id, {title: this.taskTitle, description: this.taskDescription});
+            this.clearForm();
+        },
+        clearForm() {
+            this.taskTitle = '';
+            this.taskDescription = '';
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
